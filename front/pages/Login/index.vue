c57dc8fa7ee333a8c6836cf6492dd45a56d39259
Alkiva <fernanda22mr@gmail.com>
2018-10-29 10:15:40 -0600

    login

---
+++ front/pages/Login/index.vue
@@ -0,0 +1,20 @@
+<template>
+        <div>
+    <div class="form-group">
+        <label for="usr">Usuario:</label>
+        <input type="text" class="form-control" id="usr">
+    </div>
+        <div class="form-group">
+        <label for="contr">Contraseña:</label>
+        <input type="password" class="form-control" id="contr">
+        </div>
+        </div>
+    </template>
+  
+    <script>
+
+  </script>
+  
+  <style>
+  
+  </style>
