d13555c267e9ba8204206ec224cc13f5e3b3ce14
Abigail González Hidalgo <abigailgonzalezh@gmail.com>
2018-11-05 11:12:15 -0600

    agregar horario

---
+++ front/components/Horario/Agregar.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+    <!--Empieza la form-->
+        <form>
+        <!--Tours-->
+            <div class="form-group">
+                <label for="tours">Tours</label>
+                <select class="form-control mt-3" id="toursOpciones">
+                    <option>Puebla Fascinante</option>
+                    <option>Cholula Magico</option>
+                </select>
+            </div>
+        <!--Tipo de ticket-->
+            <div class="form-group">
+                <label for="tickets">Tipo de tickets</label>
+                <select class="form-control mt-3" id="tipoTickets">
+                    <option>Adulto</option>
+                    <option>Niño</option>
+                    <option>INAPAM</option>
+                </select>
+            </div>
+        <!--Precio-->
+            <div class="form-group">
+                <label for="precios">Precio</label>
+                <textarea class="form-control" id="inputPrecio" rows="1"></textarea>
+            </div>
+        </form>
+    <!--Termina la form-->
+
+    <!--Boton Agregar-->
+        <button type="button" class="btn btn-primary mt-3">Agregar</button>
+    </div>   
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.container {
+    margin-left: 160px;
+    margin-right: 0px; /* Same as the width of the sidenav */
+    display: inline-block;
+    font-size: 20px; /* Increased text to enable scrolling */
+    text-align: center;
+    align-content: center;
+}
+</style>
+
+
