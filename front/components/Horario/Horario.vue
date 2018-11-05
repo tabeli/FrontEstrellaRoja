5d4b52bd6f41a0037fb3e8f6f52e748cbd830260
Alkiva <fernanda22mr@gmail.com>
2018-11-05 11:13:48 -0600

    Paradas

---
+++ front/components/Horario/Horario.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+      <br>
+      <div class="mt-5">
+    <div class="row">
+    <!--Buscador-->
+    <div class="col">
+      <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Ingrese texto a buscar" aria-label="Search">
+        <button class="btn btn-outline-danger my-2 my-sm-0" type="submit"><img src="@/static/magnifier.png" width="20" height="20"></button> 
+      </form>
+    </div>
+    <!--Agregar-->
+      <div class = "col text-right">
+        <nuxt-link :to="{ name: 'boletos-tipoboletoeditar' }" replace>
+        <button type="button" class="btn btn-info text-right">Agregar</button>
+        </nuxt-link> 
+      </div>
+    </div>
+    <!--Tabla-->
+    <table class="table mt-3">
+      <thead class="bg-success">
+        <tr>
+          <th scope="col">Hora</th>
+          <th scope="col">Editar</th>
+          <th scope="col">Borrar</th>
+        </tr>
+      </thead>
+        <tbody>
+          <tr>
+            <th scope="row">Niño</th>
+            <nuxt-link :to="{ name: 'boletos-tipoboletoeditar' }">
+            <td><img src="@/static/pencil.png"></td>
+            </nuxt-link>
+            <td><img src="@/static/basurero.png"></td>
+          </tr>
+        </tbody>
+    </table>
+  </div> 
+    </div>
+  </template>
+  
+  <script>
+  
+  </script>
+  
+  <style>
+  
+  </style>
