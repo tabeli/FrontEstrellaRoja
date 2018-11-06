f7c174a72a5c816f64dd4b1bfb888f6c24eda6db
Abigail González Hidalgo <abigailgonzalezh@gmail.com>
2018-11-06 12:30:00 -0600

    index de lugar

---
+++ front/pages/lugares/index.vue
@@ -0,0 +1,38 @@
+  <template>
+    <div>
+      <div class ="row">
+          <div class="col-sm-1">
+              <Sidebar/>
+          </div>
+          <div class="col-11">
+            <Header/>
+            <Lugar/>
+            <Footer/>
+          </div>
+      </div>
+    </div>
+    
+  </template>
+
+  <script>
+
+  import Sidebar from "@/components/Sidebar"
+  import Footer from "@/components/Footer"
+  import Header from "@/components/Header"
+  import Lugar from "@/components/Lugares/Lugar"
+
+  export default {
+    components: {
+      Sidebar,
+      Header,
+      Footer,
+      Lugar
+    }
+  }
+  </script>
+
+  <style>
+
+  </style>
+
+
