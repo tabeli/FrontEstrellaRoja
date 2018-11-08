3038ef2378f53045db18ab9419ba085370062345
Alkiva <fernanda22mr@gmail.com>
2018-11-08 17:21:32 -0600

    sombra

--- front/components/Mapa/GoogleMap.vue
+++ front/components/Mapa/GoogleMap.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="container">
     <div class="form-group mr-sm-2">
-      <label for="exampleFormControlSelect1">Tours</label>
-      <select class="form-control" id="exampleFormControlSelect1">
+
+ <!--cambiar ruta-->     
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <label for="exampleFormControlSelect1">Seleccione uno de los tours</label>
+      <select class="form-control shadow-sm p-3 mb-5 bg-white rounded" id="exampleFormControlSelect1">
+        <option>Puebla fascinante</option>
+        <option>Cholula</option>
       </select>
+    </div>
+
+    <div class="col-sm"></div>
+    <div class="col-sm"></div>
+  </div>
+</div>
+
+
     </div>
     <br>
     <gmap-map
@@ -81,4 +96,55 @@ export default {
     }
   }
 };
+
+window.onload = function () {
+           var mapOptions = {
+               center: new google.maps.LatLng(markers[0].lat, markers[0].lng),
+               zoom: 8,
+               mapTypeId: google.maps.MapTypeId.ROADMAP
+           };
+           var path = new google.maps.MVCArray();
+           var service = new google.maps.DirectionsService();
+ 
+           var infoWindow = new google.maps.InfoWindow();
+           var map = new google.maps.Map(document.getElementById("dvMap"), mapOptions);
+           var poly = new google.maps.Polyline({ map: map, strokeColor: '#FF8200' });
+           var lat_lng = new Array();
+           for (i = 0; i < markers.length; i++) {
+               var data = markers[i]
+               var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+               lat_lng.push(myLatlng);
+               var marker = new google.maps.Marker({
+                   position: myLatlng,
+                   map: map,
+                   title: data.title
+               });
+               (function (marker, data) {
+                   google.maps.event.addListener(marker, "click", function (e) {
+                       infoWindow.setContent(data.description);
+                       infoWindow.open(map, marker);
+                   });
+               })(marker, data);
+           }
+           for (var i = 0; i < lat_lng.length; i++) {
+               if ((i + 1) < lat_lng.length) {
+                   var src = lat_lng[i];
+                   var des = lat_lng[i + 1];
+                   path.push(src);
+                   poly.setPath(path);
+                   service.route({
+                       origin: src,
+                       destination: des,
+                       travelMode: google.maps.DirectionsTravelMode.DRIVING
+                   }, function (result, status) {
+                       if (status == google.maps.DirectionsStatus.OK) {
+                           for (var i = 0, len = result.routes[0].overview_path.length; i < len; i++) {
+                               path.push(result.routes[0].overview_path[i]);
+                           }
+                       }
+                   });
+               }
+           }
+       }
+
 </script>
