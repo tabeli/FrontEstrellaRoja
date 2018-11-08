1cb88ec55e31c348b7a459ee56c4c969f17bf1c3
Abigail González Hidalgo <abigailgonzalezh@gmail.com>
2018-11-08 17:40:43 -0600

    agregar boletos

---
+++ front/pages/boletos/agregar.vue
@@ -0,0 +1,36 @@
+  <template>
+    <div>
+      <div class ="row">
+          <div class="col-sm-1">
+              <Sidebar/>
+          </div>
+          <div class="col-11">
+            <Header/>
+            <Agregar/>
+            <Footer/>
+          </div>
+      </div>
+    </div>
+    
+  </template>
+
+  <script>
+
+  import Sidebar from "@/components/Sidebar"
+  import Footer from "@/components/Footer"
+  import Header from "@/components/Header"
+  import Agregar from "@/components/Boleto/Agregar"
+
+  export default {
+    components: {
+      Sidebar,
+      Header,
+      Footer,
+      Agregar
+    }
+  }
+  </script>
+
+  <style>
+
+  </style>
