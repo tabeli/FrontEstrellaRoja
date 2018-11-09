b803927db89cb9987b47f13befb44ac3f1441785
AndreRdz7 <A01730716@itesm.mx>
2018-11-09 01:13:42 -0600

    update camion

--- front/pages/camiones/Agregar.vue
+++ front/pages/camiones/Agregar.vue
@@ -1,34 +1,50 @@
 <template>
-    <div>
+   <div>
+
+      <div class="Header">
+        <Header/>
+      </div>
+
       <div class ="row">
-          <div class="col-sm-1">
+          <div class="col-sm-1 mt-5">
               <Sidebar/>
           </div>
-          <div class="col-11">
-            <Header/>
-            <Agregar/>
-            <Footer/>
+          <div class="col-sm-11">
+              <Agregar :idBus="idBus"/>
           </div>
       </div>
+
+    <div class="Footer mt-5">
+      <Footer/>
+    </div>
+
     </div>
   </template>
   
   <script>
-    import Sidebar from '@/components/Sidebar'
-    import Agregar from '@/components/Camion/Agregar'
-    import Header from '@/components/Header'
-    import Footer from '@/components/Footer'
-  
-    export default {
-      components: {
-        Sidebar,
-        Agregar,
-        Header,
-        Footer
-      }
-    }
-  </script>
+import Sidebar from "@/components/Sidebar";
+import Agregar from "@/components/Camion/Agregar";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default {
+  data: function() {
+    return {
+      idBus: this.$route.params.idBus
+    };
+  },
+  components: {
+    Sidebar,
+    Agregar,
+    Header,
+    Footer
+  },
+  created: function() {
+    console.log("id of bus: ");
+    console.log(this.idBus);
+  }
+};
+</script>
   
   <style>
-  
-  </style>
+</style>
